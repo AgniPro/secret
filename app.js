b538a1b880873e2c3162bdfe7d35593948b739f4
AgniPro <84276343+AgniPro@users.noreply.github.com>
2023-04-30 16:49:28 +0530

    new testing

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const sesssion = require("express-session");
@@ -9,6 +10,12 @@ const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const findOrCreate = require("mongoose-findorcreate");
+const _ = require("lodash");
+
+const homeStartingContent = "Application are under progress so please visit to learngraduation.blogspot.com or click on below link"
+const contactContent = "Contact us at agnipro(at)gmail(dot)com"
+const aboutContent = "this is blog post templet designed by agnipro and the creator is abhidhek mehta"
+
 
 const app = express();
 
@@ -18,27 +25,58 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(cookieParser());
 
 app.use(sesssion({
     secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: false
+    resave: true, // default to false
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60 * 60 * 1000
+    } //1 hour
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-
+// Connection to database
 
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb+srv://"+ process.env.DBPAS +".absogmm.mongodb.net/secret");
 
-const userSchema = new mongoose.Schema ({
-    email: String,
-    password: String,
-    googleId: String,
-    secret: String
-  });
+// mongodb+srv://"+ process.env.DBPAS +".absogmm.mongodb.net/learngraduation    || mongodb://127.0.0.1:27017/learngraduation
+mongoose.connect("mongodb+srv://" + process.env.DBPAS + ".absogmm.mongodb.net/learngraduation");
+
+// Authentication section
+
+const userSchema = new mongoose.Schema({
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            min: 3,
+            max: 20
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 3,
+            max: 10
+        },
+        googleId: String
+
+    },
+
+    {
+        timestamps: true
+    }
+
+);
 
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
@@ -56,11 +94,10 @@ passport.deserializeUser(function (id, done) {
     });
 });
 
-
 passport.use(new GoogleStrategy({
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/secrets",
+        callbackURL: process.env.CALLBACK_URL,
         passReqToCallback: true
     },
     function (request, accessToken, refreshToken, profile, cb) {
@@ -69,23 +106,20 @@ passport.use(new GoogleStrategy({
         }, function (err, user) {
             return cb(err, user);
         });
-    }
-));
-
-app.get("/", function (req, res) {
-    res.render("home");
-});
+    }));
 
 app.get("/auth/google",
     passport.authenticate("google", {
         scope: ["email", "profile"]
-    }));
+    })
+);
 
-app.get("/auth/google/secrets",
+app.get("/auth/google/compose",
     passport.authenticate("google", {
-        successRedirect: "/secrets",
+        successRedirect: "/dashboard",
         failureRedirect: "/login"
-    }));
+    })
+);
 
 app.get("/login", function (req, res) {
     res.render("login");
@@ -96,27 +130,6 @@ app.get("/register", function (req, res) {
 
 });
 
-app.get("/secrets", function (req, res) {
-   User.find({"secret":{$ne:null}}, function(err, foundUser){
-    if(err){
-        console.log(err);
-    }else{
-        if (foundUser){
-            res.render("secrets", {usersWithSecrets: foundUser})
-        }
-    }
-   });
-
-});
-
-app.get("/submit", function (req, res) {
-    if (req.isAuthenticated()) {
-        res.render("submit");
-    } else {
-        res.redirect("/login");
-    }
-
-});
 
 app.get("/logout", function (req, res) {
     req.logout(function (err) {
@@ -128,6 +141,7 @@ app.get("/logout", function (req, res) {
 
 });
 
+
 app.post("/register", function (req, res) {
     User.register({
         username: req.body.username
@@ -137,7 +151,7 @@ app.post("/register", function (req, res) {
             res.redirect("/register");
         } else {
             passport.authenticate("local")(req, res, function () {
-                res.redirect("/secrets");
+                res.redirect("/");
             })
         }
     })
@@ -152,35 +166,222 @@ app.post("/login", function (req, res) {
     req.login(user, function (err) {
         if (err) {
             console.log(err);
+            res.redirect("/login");
         } else {
             passport.authenticate("local")(req, res, function () {
-                res.redirect("secrets");
+                res.redirect("/");
             });
         }
     });
 });
 
-app.post("/submit", function(req, res){
-    const  submittedSecret = req.body.secret;
-  
-  //Once the user is authenticated and their session gets saved, their user details are saved to req.user.
-    // console.log(req.user.id);
-  
-    User.findById(req.user.id, function(err, foundUser){
-      if (err) {
-        console.log(err);
-      } else {
-        if (foundUser) {
-          foundUser.secret = submittedSecret;
-          foundUser.save(function(){
-            res.redirect("/secrets");
-          });
-        }
-      }
+
+// Main blog Routes
+
+const PostSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: String,
+    purl: {
+        type: String,
+        required: true,
+    },
+    disc: String,
+    thumbnail: String,
+    content: String,
+    pdate: String,
+
+}, {
+    timestamps: true
+});
+
+const Post = new mongoose.model("Post", PostSchema);
+
+
+
+app.get("/", function (req, res) {
+
+    Post.find({}, function (err, posts) {
+        res.render("home", {
+            startingContent: homeStartingContent,
+            posts: posts,
+        });
+    }).sort({
+        _id: -1
+    }).limit(6);
+
+});
+
+app.get("/contact", function (req, res) {
+    res.render("pages/contact", {
+        contactpg: contactContent
     });
-  });
+
+});
+app.get("/about", function (req, res) {
+    res.render("pages/about", {
+        aboutpg: aboutContent
+    });
+
+});
+
+
+app.get("/search", function (req, res) {
+
+    const key = new RegExp(escapeRegex(req.query.q), 'gi');
+    Post.find({
+        title: key
+    }, function (err, articles) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (articles.length < 1) {
+                res.redirect("/");
+            } else {
+                res.render("search", {
+                    articles: articles,
+                });
+            }
+        }
+    }).limit(6);
+
+});
+
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
+
+// For admin users 
+
+app.get("/dashboard", function (req, res) {
+    if (req.isAuthenticated()) {
+
+        const userid = new RegExp(escapeRegex(req.user.username), 'gi');
+        Post.find({
+            author: userid
+        }, function (err, userPosts) {
+            if (err) {
+                console.log(err);
+            } else {
+                if (userPosts) {
+                    if (userPosts < 1) {
+                        res.render("dashboard", {
+                            userPosts: [{
+                                title: "you havent post  yet",
+                                purl: "",
+                                disc: "you are creater"
+                            }],
+                            userId: req.user.username
+                        });
+                    } else {
+                        res.render("dashboard", {
+                            userPosts: userPosts,
+                            userId: req.user.username
+                        });
+                    }
+                }
+            }
+        }).sort({
+            _id: -1
+        }).limit(6);;
+
+    } else {
+        res.redirect("/login");
+    }
+});
+
+
+// update post content
+
+app.post("/update", function (req, res) {
+    const submittedPost = req.body.pContent;
+    const purl = req.body.purl
+    Post.findOneAndUpdate({
+        "purl": purl
+    }, {
+        $inc: {
+            "purl": req.body.pUrl,
+            "title": req.body.pTitle,
+            "disc": req.body.pDisc,
+            "thumbnail": req.body.thumbnail,
+            "content": req.body.pContent,
+            "pdate": date()
+
+        }
+    })
+
+
+});
+
+
+app.get("/compose", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("compose");
+    } else {
+        res.redirect("/login");
+    }
+
+});
+
+function date() {
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    var date = new Date();
+    var month = date.getMonth();
+    var tmonth = months[month];
+
+    return todayDate = date.getDate() + " " + tmonth + ", " + date.getFullYear();
+};
+
+app.post("/submit", function (req, res) {
+
+    const post = new Post({
+        author: req.user.username,
+        purl: req.body.pUrl,
+        title: req.body.pTitle,
+        disc: req.body.pDisc,
+        thumbnail: req.body.thumbnail,
+        content: req.body.pContent,
+        pdate: date()
+    });
+
+    if (req.isAuthenticated()) {
+        post.save(function (err) {
+            if (!err) {
+                res.redirect("/");
+            }
+        });
+    } else {
+        res.redirect("/login")
+    }
+
+});
+
+app.get("/posts/:postUrl", function (req, res) {
+
+    const requestedPostUrl = req.params.postUrl;
+
+    Post.findOne({
+        purl: requestedPostUrl
+    }, function (err, post) {
+        res.render("post", {
+
+            purl: post.purl,
+            title: post.title,
+            disc: post.disc,
+            thumbnail: post.thumbnail,
+            content: post.content,
+            pdate: post.pdate
+
+        });
+    });
+
+});
+
 
 app.listen(3000, function () {
-    console.log("server is started");
+    console.log("server has started");
 
 });
